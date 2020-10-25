a185c308c596a7e92b9d0b02f63cefc1d7b02ba2
bhargavmln <bhargavmln@gmail.com>
2020-10-25 17:21:15 +0530

    [bhargavmln]create:maven project for address book created

---
+++ src/main/java/com/capg/addressbook/AddressBookMain.java
@@ -0,0 +1,172 @@
+package com.capg.addressbook;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class AddressBookMain {
+	static Scanner sc = new Scanner(System.in);
+	AddressBookIO addressBookIO=new AddressBookIO();
+	static ArrayList<Contact> contactList;
+	public Map<String, Contact> nameToContactMap;
+	public Map<String, List<Contact>> cityToContactMap;
+	public Map<String, List<Contact>> stateToContactMap;
+
+	public AddressBookMain() {
+		contactList = new ArrayList<>();
+		nameToContactMap = new LinkedHashMap<String, Contact>();
+	}
+
+	public List<Contact> getcontactArray() {
+		return contactList;
+	}
+
+	public Map<String, Contact> getcontactMap() {
+		return nameToContactMap;
+	}
+
+	public void addContact() {
+		String firstName = "";
+		String lastName = "";
+		while (true) {
+			System.out.println("Enter First Name");
+			firstName = sc.next();
+			System.out.println("Enter last Name");
+			lastName = sc.next();
+			if (checkForDuplicate(firstName, lastName))
+				continue;
+			else
+				break;
+		}
+		System.out.println("Enter the Address");
+		String address = sc.next();
+		System.out.println("Enter the City");
+		String city = sc.next();
+		System.out.println("Enter the State");
+		String state = sc.next();
+		System.out.println("Enter the Pincode");
+		int pinCode = sc.nextInt();
+		System.out.println("Enter the Number");
+		long mobile = sc.nextLong();
+		System.out.println("Enter the Email");
+		String email = sc.next();
+		Contact contact = new Contact(firstName, lastName, address, city, state, pinCode, mobile, email);
+		contactList.add(contact);
+		String name = firstName + " " + lastName;
+		nameToContactMap.put(name, contact);
+	}
+
+	public void printContacts() {
+		System.out.println(contactList);
+	}
+
+	public void editContact() {
+		System.out.println("Enter The First Name of the contact to be edited");
+		String firstName = sc.next();
+		System.out.println("Enter The last Name of the contact to be edited");
+		String lastName = sc.next();
+		String name = firstName + " " + lastName;
+		Contact contact = nameToContactMap.get(name);
+		System.out.print("Enter address,city,state,pincode,phonenumber,email");
+		String address = sc.next();
+		contact.setAddress(address);
+		String city = sc.next();
+		contact.setCity(city);
+		String state = sc.next();
+		contact.setState(state);
+		int pin = sc.nextInt();
+		contact.setPinCode(pin);
+		long mobile = sc.nextLong();
+		contact.setPhoneNumber(mobile);
+		String email = sc.next();
+		contact.setEmailId(email);
+	}
+
+	public void deleteContact() {
+		System.out.println("Enter The First Name to delete the contact details");
+		String firstName = sc.next();
+		System.out.println("Enter The last Name to delete the contact details");
+		String lastName = sc.next();
+		String name = firstName + " " + lastName;
+		Contact contact = nameToContactMap.get(name);
+		contactList.remove(contact);
+		nameToContactMap.remove(name);
+		System.out.println("Contact deleted");
+
+	}
+
+	public boolean checkForDuplicate(String firstname, String lastname) {
+		if (contactList.stream().anyMatch(c -> c.getFirstName().equals(firstname))
+				&& contactList.stream().anyMatch(c -> c.getLastName().equals(lastname))) {
+			System.out.println("This contact already exists.");
+			return true;
+		} else
+			return false;
+	}
+
+	public void sortByName() {
+		List<Contact> sortedList = contactList.stream().sorted(Comparator.comparing(Contact::getFirstName))
+				.collect(Collectors.toList());
+		sortedList.forEach(System.out::println);
+	}
+
+	public void sortByCity() {
+		List<Contact> sortedList = contactList.stream().sorted(Comparator.comparing(Contact::getCity))
+				.collect(Collectors.toList());
+		sortedList.forEach(System.out::println);
+	}
+
+	public void sortByState() {
+		List<Contact> sortedList = contactList.stream().sorted(Comparator.comparing(Contact::getState))
+				.collect(Collectors.toList());
+		sortedList.forEach(System.out::println);
+	}
+
+	public void manageAddressBook() {
+		boolean check = true;
+		while (check == true) {
+			System.out.println("Please select your choice" + "\n1. Add Contact Details" + "\n2. Edit Contact Details"
+					+ "\n3. Delete Contact Details" + "\n4. Show Contact details" + "\n5. Sort details by name"
+					+ "\n6. Sort details by state" + "\n7. Sort details by city" + "\n8. Exit");
+
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				addContact();
+				break;
+			case 2:
+				if (contactList.size() == 0)
+					System.out.println("Plese Enter contacts");
+				else
+					editContact();
+				break;
+			case 3:
+				if (contactList.size() == 0)
+					System.out.println("Plese Enter contacts");
+				else
+					deleteContact();
+				break;
+			case 4:
+				printContacts();
+				break;
+			case 5:
+				sortByName();
+				break;
+			case 6:
+				sortByState();
+				break;
+			case 7:
+				sortByCity();
+				break;
+			case 8:
+				System.out.println("Exit");
+				check = false;
+				break;
+			}
+		}
+	}
+}
---
+++ src/main/java/com/capg/addressbook/AddressBookManagement.java
@@ -0,0 +1,115 @@
+package com.capg.addressbook;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class AddressBookManagement {
+	static Scanner sc = new Scanner(System.in);
+	public static Map<String, AddressBookMain> addressBookMap = new TreeMap<>();
+	public static Map<String, Contact> cityContactMap = new TreeMap<>();
+	public static Map<String, Contact> stateContactMap = new TreeMap<>();
+
+	public static void addAddressBook() {
+		AddressBookMain addressBook = new AddressBookMain();
+		System.out.println("Enter the name of address book");
+		String name = sc.next();
+		addressBook.manageAddressBook();
+		addressBookMap.put(name, addressBook);
+	}
+
+	public static void showContactGroupedByCity() {
+		Set<String> listOfCity = cityContactMap.keySet();
+		for (String city : listOfCity) {
+			System.out.println("Contact Entries for CITY: " + city);
+			searchPersonInBook(city);
+		}
+	}
+
+	public static void showContactGroupedByState() {
+		Set<String> listOfState = stateContactMap.keySet();
+		for (String state : listOfState) {
+			System.out.println("Contact Entries for STATE: " + state);
+			searchPersonInBook(state);
+		}
+	}
+
+	public static void searchPersonInBook(String searchIn) {
+		Predicate<Contact> search = n -> n.getFirstName().equals(searchIn) ? true : false;
+		Consumer<Contact> display = n -> System.out.println(n);
+		addressBookMap.forEach((k, v) -> {
+			v.getcontactArray().stream().filter(search).forEach(display);
+		});
+	}
+
+	public static void countByCity() {
+		Set<String> listOfCity = cityContactMap.keySet();
+		for (String cityName : listOfCity) {
+			Contact contactNumber = cityContactMap.get(cityName);
+			System.out.println("No of contact entries for CITY " + cityName + " "
+					+ ((Map<String, AddressBookMain>) contactNumber).size());
+		}
+	}
+
+	public static void countByState() {
+		Set<String> listOfState = stateContactMap.keySet();
+		for (String stateName : listOfState) {
+			Contact contactNumber = stateContactMap.get(stateName);
+			System.out.println("No of contact entries for STATE " + stateName + " "
+					+ ((Map<String, AddressBookMain>) contactNumber).size());
+		}
+	}
+
+	public static void main(String args[]) {
+		AddressBookManagement aBookManager = new AddressBookManagement();
+		while (true) {
+			System.out.println("\n Please Enter your choice" + "\n1. Add a new Address Book"
+					+ "\n2. Search person across all address books" + "\n3. Show contact  by city"
+					+ "\n4. Show contacts by state" + "\n5. count contacts  by city" + "\n6. count contacts by state"
+					+ "\n7. Exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				while (true) {
+					AddressBookMain m = new AddressBookMain();
+					System.out.println("Enter A name for address Book");
+					String name = sc.next();
+					if (aBookManager.addressBookMap.containsKey(name)) {
+						System.out.println("\nAddress Book already exists !!!\n");
+						continue;
+					} else {
+						aBookManager.addressBookMap.put(name, m);
+						System.out.println("\n In Address Book : " + name);
+						m.manageAddressBook();
+						break;
+					}
+				}
+				break;
+
+			case 2:
+				System.out.println("Enter city or state to search a person");
+				String search = sc.next();
+				aBookManager.searchPersonInBook(search);
+				break;
+			case 3:
+				showContactGroupedByCity();
+				break;
+			case 4:
+				showContactGroupedByState();
+				break;
+			case 5:
+				countByCity();
+				break;
+			case 6:
+				countByState();
+				break;
+			case 7:
+				System.exit(0);
+				break;
+			}
+		}
+	}
+}
